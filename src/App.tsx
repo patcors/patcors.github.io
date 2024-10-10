04f6855df76d9e91bb15052558d522da691b17ba
Patrick Corbett <patrickcorbett12@gmail.com>
2024-10-10 23:50:55 +1100

    Add birthday

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import confetti from "canvas-confetti"
 
 function App() {
   
-
+  const date = new Date()
+  const isAmeliasBirthday = date.getDate() == 13 && date.getMonth() == 9
   return (
     <>
       <div onClick={() => confetti()}>
           <img src="/amelia.jpg" className="logo amelia" alt="Amelia Sucks" />
       </div>
-      <h1>AMELIA SUCKS</h1>
+      {isAmeliasBirthday ? <div><h1>AMELIA DOESNT SUCK</h1><h3>Happy Birthday Amelia!</h3></div>: <h1>AMELIA SUCKS</h1>}
+      
     </>
   )
 }
